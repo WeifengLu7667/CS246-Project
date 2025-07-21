7d1ddf9e857a8c4ff3ffea5cc8cdcc66ea655a1d
Weifeng Lu <weifenglu7667@gmail.com>
2025-07-21 17:05:07 -0400

    Modify piece.h

--- piece.h
+++ piece.h
@@ -2,9 +2,19 @@
 #define PIECE_H
 
 #include <vector>
+#include "posn.h"
 using namespace std;
 
 class Piece {
+protected:
+    Posn posn;
+    char symbol;
+    int color;
+
+public:
+    // ctor
+    Piece(int row, int col, char symbol, int color);
+
 
 
 };
