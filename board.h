f1b4d9db77cb70b876278b258debec9e9ced1dc9
Paddy Wen <wenp@eduroam-campus-10-36-69-79.campus-dynamic.uwaterloo.ca>
2025-07-21 17:36:01 -0400

    updates

--- board.h
+++ board.h
@@ -2,12 +2,17 @@
 #define BOARD_H
 
 #include "subject.h"
+#include "piece.h"
 #include <vector>
 #include <memory>
 
 
 class Board: public Subject {
-    std::vector<std::unique_ptr<Piece>>
+    std::vector<vector<std::unique_ptr<Piece>>> board;
+    public:
+    Board() {};
+    
+
 };
 
 #endif
