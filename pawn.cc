81198fb843c847af191ff1477cff752589a35788
Weifeng Lu <weifenglu7667@gmail.com>
2025-07-21 19:56:45 -0400

    Add pawn.cc

---
+++ pawn.cc
@@ -0,0 +1,15 @@
+#include "pawn.h"
+#include "board.h"
+
+Pawn::Pawn(Colour c) : Piece{c} {}
+
+char Pawn::getSymbol() const {
+    return colour == Colour::White ? 'P' : 'p';
+}
+
+std::vector<Posn> Pawn::getValidMoves(const Board& b, Posn p) const {
+    std::vector<Posn> validMoves;
+    
+
+
+}
