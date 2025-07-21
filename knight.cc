87cc9df0283dd25e47f7f0a48b24af1fd3e71490
Weifeng Lu <weifenglu7667@gmail.com>
2025-07-21 18:25:58 -0400

    Finish knight.cc

--- knight.cc
+++ knight.cc
@@ -13,6 +13,13 @@ std::vector<Posn> Knight::getValidMoves(const Board& b, Posn p) const {
     const int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
     const int dy[] = {1, 2, 2, 1, -1, -2, -2, -1};
 
+    for (int i = 0; i < 8; i++) {
+        int newRow = p.row + dx[i];
+        int newCol = p.col + dy[i];
 
-
+        if (newRow >= 0 && newRow <=7 && newCol >= 0 && newCol <=7) {
+            validMoves.emplace_back(Posn{newRow, newCol});
+        }
+    }
+    return validMoves;
 }
