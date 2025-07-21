1559cf378a8cd019b5d7b6e2498a01ccb386c166
Austin Ma <aaustinmaa@gmail.com>
2025-07-21 18:29:49 -0400

    change indentation

--- scoreboard.h
+++ scoreboard.h
@@ -7,12 +7,12 @@ class Scoreboard {
     double whiteScore;
     double blackScore;
 
-    public:
-        void startGame();
-        void endGame();
-        void setWhiteScore(double score);
-        void setBlackScore(double score);
-        void printScore(std::ostream &out) const; 
+public:
+    void startGame();
+    void endGame();
+    void setWhiteScore(double score);
+    void setBlackScore(double score);
+    void printScore(std::ostream &out) const; 
 };
 
 #endif
