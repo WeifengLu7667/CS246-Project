4bbed96069050887f11304c6dd28568a604cd682
Weifeng Lu <weifenglu7667@gmail.com>
2025-07-21 17:39:07 -0400

    Add king.h

--- king.h
+++ king.h
@@ -1,12 +1,12 @@
-#ifndef KNIGHT_H
-#define KNIGHT_H
+#ifndef KING_H
+#define KING_H
 
 #include "piece.h"
 #include "posn.h"
 
-class Knight : public Piece {
+class King : public Piece {
 public:
-    Knight(Colour c);
+    King(Colour c);
 
     char getSymbol() const override;
     std::vector<Posn> getValidMoves(const Board& b, Posn p) const override;
