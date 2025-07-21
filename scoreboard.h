cbbd6148c00feca379e09bbc6bd10c0cefff3ec9
Austin Ma <aaustinmaa@gmail.com>
2025-07-21 18:17:30 -0400

    added scoreboard.h

---
+++ scoreboard.h
@@ -0,0 +1,18 @@
+#ifndef SCOREBOARD_H
+#define SCOREBOARD_H
+
+
+
+class Scoreboard {
+    double whiteScore;
+    double blackScore;
+
+    public:
+        void startGame();
+        void endGame();
+        void setWhiteScore(double score);
+        void setBlackScore(double score);
+        void printScore(std::ostream &out) const; 
+};
+
+#endif
