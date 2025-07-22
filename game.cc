68931afcdb5ce8e7e9e4059783494c2f0ad266c7
Austin Ma <aaustinmaa@gmail.com>
2025-07-22 17:15:26 -0400

    writing game.cc

--- game.cc
+++ game.cc
@@ -18,19 +18,39 @@ void Game::gameRun() {
             Colour turn;
             bool oneWhiteKing = false;
             bool oneBlackKing = false;
-            vector<vector<char>> board(8, vector<char>(8));
+            CastlingInfo castlingRights{false, false, false, false};
+            Posn enPassantTarget{-1, -1};
+            vector<vector<char>> setupBoard(8, vector<char>(8));
             while (getline(cin, line)) {
                 
-                if (line == "done") {
-                    CastlingInfo castlingInfo{false, false, false, false};
-                    Posn enPassantTarget;
-                    //add code here
+                if (line == "done") { // if requirements are met, we can exit setup mode and start the game
+                    // we assume castling and en passant are invalid
+
                     if (oneWhiteKing && oneBlackKing) {// first check if both kings are present
                         Board tempBoard;
-                        State state{turn, State::Status::IN_PROGRESS, };
-                        tempBoard.changeState()
+                        State state{turn, GameStatus::IN_PROGRESS, castlingRights, enPassantTarget, setupBoard};
+                        tempBoard.changeState(state);
+                        if (tempBoard.isCheck(Colour::White) || tempBoard.isCheck(Colour::Black)) {
+                            cout << "invalid board, can't exit. at least one king is in check" << endl;
+                        } else {
+                            bool valid = true;
+                            for (int i = 0; i < 8; i++) {
+                                if (setupBoard[0][i] == 'p' || setupBoard[0][i] == 'P' || setupBoard[7][i] == 'p' || setupBoard[7][i] == 'P') {
+                                    cout << "invalid board, can't exit. pawns are not allowed in the first or last row" << endl;
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                            if (valid) {
+                                // all requirements are met, we can exit setup mode and start the game
+                                board.changeState(state);
+                            }    
+                        }
+                    } else {
+                        cout << "invalid board, can't exit. King number is not correct" << endl;
+                    }
 
-                    break;
+
                 } else if (line == "= black") {
                     turn = Colour::Black;
                 } else if (line == "= white") {
