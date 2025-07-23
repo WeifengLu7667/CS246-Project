90abbc62f42b8b1de85c5ee39f2583c2aee3fde6
Weifeng Lu <weifenglu7667@gmail.com>
2025-07-23 18:12:57 -0400

    Add check move to level2.cc

--- level2.cc
+++ level2.cc
@@ -3,34 +3,52 @@
 #include "board.h"
 #include <random>
 
+#include "level2.h"
+#include "board.h"
+#include "move.h"
+#include <vector>
+#include <random>
+#include <ctime>
+
 Move Level2::chooseMove(Board& board, Colour colour) {
-
-    // Get all valid moves
     std::vector<Move> allMoves = board.legalMoves(colour);
-    int numMoves = allMoves.size();
+    Colour opponent = (colour == Colour::White) ? Colour::Black : Colour::White;
 
-    // Try capture
-    for (int i = 0; i < numMoves; i++) {
-        Posn target = allMoves[i].to;
-        Piece* targetPiece = board.getPieceAt(target.row, target.col);
-        if (targetPiece != nullptr && targetPiece->getColour() != colour) {
-            return allMoves[i];
+    std::vector<Move> mateMoves;
+    std::vector<Move> checkMoves;
+    std::vector<Move> captureMoves;
+
+    for (const Move& mv : allMoves) {
+        Board copy = board;
+        copy.movePiece(mv);
+
+        if (copy.isCheckMate(opponent)) {
+            mateMoves.push_back(mv);
+        } else if (copy.isCheck(opponent)) {
+            checkMoves.push_back(mv);
+        } else {
+            Piece* target = board.getPieceAt(mv.to.row, mv.to.col);
+            if (target != nullptr && target->getColour() != colour) {
+                captureMoves.push_back(mv);
+            }
         }
     }
 
-    // No capture possible, randomly pick a move
     static bool seeded = false;
     if (!seeded) {
         std::srand(std::time(nullptr));
         seeded = true;
     }
-    int index = std::rand() % numMoves;
 
-    return allMoves[index];
+    if (!mateMoves.empty()) {
+        return mateMoves[std::rand() % mateMoves.size()];
+    }
+    if (!checkMoves.empty()) {
+        return checkMoves[std::rand() % checkMoves.size()];
+    }
+    if (!captureMoves.empty()) {
+        return captureMoves[std::rand() % captureMoves.size()];
+    }
+
+    return allMoves[std::rand() % allMoves.size()];
 }
-
-/*
-Features TO BE ADDED:
-    Prefer Check > normal Cpature
-    Prefer Capture Higher-value pieces > Capture Lower-value pieces
-*/
