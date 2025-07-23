861aac39a2ed09248699759996500bdeb0c67a63
Weifeng Lu <weifenglu7667@gmail.com>
2025-07-22 20:40:33 -0400

    Finish level1.cc

--- level1.cc
+++ level1.cc
@@ -1,7 +1,20 @@
-#include "level1.h";
+#include "level1.h"
+#include "move.h"
+#include "board.h"
 #include <random>
 
 Move Level1::chooseMove(Board& board, Colour colour) {
     std::vector<Move> allMoves = board.legalMoves(colour);
 
+    int numMoves = allMoves.size();
+
+    // Generate a random number within the range
+    static bool seeded = false;
+    if (!seeded) {
+        std::srand(std::time(nullptr));
+        seeded = true;
+    }
+    int index = std::rand() % numMoves;
+
+    return allMoves[index];
 }
