1238e913e1cff45d9d4ae7cfa4b64cecf8169130
Austin Ma <aaustinmaa@gmail.com>
2025-07-23 20:42:00 -0400

    debug and edited game.h.cc

--- game.cc
+++ game.cc
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <string>
 #include <memory>
-#include "colour.h"
+#include <stdexcept>
 #include <vector>
+#include <sstream>
+#include "colour.h"
 #include "board.h"
 #include "humanplayer.h"
 #include "computerplayer.h"
@@ -12,6 +14,9 @@
 #include "level1.h"
 #include "level2.h"
 #include "level3.h"
+#include "posn.h"
+#include "castlinginfo.h"
+#include "state.h"
 
 using namespace std;
 
@@ -47,6 +52,203 @@ void Game::displayBoard(const std::vector<std::vector<char>>& setupBoard) {
     cout << endl << "  abcdefgh" << endl << endl;
 }
 
+void Game::resigning() {
+    State currentState = board.getGameState();
+    if (currentState.turn == Colour::White) {
+        cout << "Black wins!" << endl;
+        scoreboard.addBlackScore(1);
+    } else {
+        cout << "White wins!" << endl;
+        scoreboard.addWhiteScore(1);
+    }
+    isRunning = false;
+    useDefaultBoard = true;
+}
+
+void Game::setupMode(string line) {
+    // setup mode
+    Colour turn = Colour::White;
+    CastlingInfo castlingRights{false, false, false, false};
+    Posn enPassantTarget{-1, -1};
+    vector<vector<char>> setupBoard(8, vector<char>(8));
+    // initialize the empty board
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if ((i + j) % 2 == 0) {
+                setupBoard[i][j] = ' ';
+            } else {
+                setupBoard[i][j] = '_'; // black space
+            }
+        }
+    }
+    displayBoard(setupBoard);
+    while (getline(cin, line)) {                
+        if (line == "done") { // if requirements are met, we can exit setup mode and start the game
+            // we assume castling and en passant are invalid
+            
+            // Count kings on the board
+            int whiteKingCount = 0;
+            int blackKingCount = 0;
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (setupBoard[i][j] == 'K') whiteKingCount++;
+                    if (setupBoard[i][j] == 'k') blackKingCount++;
+                }
+            }
+
+            if (whiteKingCount == 1 && blackKingCount == 1) {// first check if both kings are present
+                Board tempBoard;
+                State state{turn, GameStatus::IN_PROGRESS, castlingRights, enPassantTarget, setupBoard};
+                tempBoard.changeState(state);
+                if (tempBoard.isCheck(Colour::White) || tempBoard.isCheck(Colour::Black)) {
+                    cout << "invalid board, can't exit. at least one king is in check" << endl;
+                } else {
+                    bool valid = true;
+                    for (int i = 0; i < 8; i++) {
+                        if (setupBoard[0][i] == 'p' || setupBoard[0][i] == 'P' || setupBoard[7][i] == 'p' || setupBoard[7][i] == 'P') {
+                            cout << "invalid board, can't exit. pawns are not allowed in the first or last row" << endl;
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid) {
+                        // all requirements are met, we can exit setup mode and start the game
+                        board.changeState(state);
+                        useDefaultBoard = false;
+                        return;
+                    }    
+                }
+            } else {
+                cout << "invalid board, can't exit. Must have exactly one king of each color (found " 
+                        << whiteKingCount << " white kings, " << blackKingCount << " black kings)" << endl;
+            }
+        } else if (line == "= black") {
+            turn = Colour::Black;
+        } else if (line == "= white") {
+            turn = Colour::White;
+        } else if (line[0] == '-') {
+            try {
+                Posn pieceToRemove = convertToPosn(line.substr(2, 4));
+                if (setupBoard[pieceToRemove.row][pieceToRemove.col] != '_' && setupBoard[pieceToRemove.row][pieceToRemove.col] != ' ') {
+                    if ((pieceToRemove.row + pieceToRemove.col) % 2 == 0) {
+                        setupBoard[pieceToRemove.row][pieceToRemove.col] = ' ';
+                    } else {
+                        setupBoard[pieceToRemove.row][pieceToRemove.col] = '_'; // black space
+                    }
+                    // display the board
+                    displayBoard(setupBoard);
+                } else {
+                    cout << "the position is already empty" << endl;
+                }
+            } catch (const invalid_argument&) {
+                cout << "invalid command in setup mode" << endl;
+            }
+        } else if (line[0] == '+') {
+            try {
+                Posn pieceToAdd = convertToPosn(line.substr(4, 6));
+                char pieceSymbol = line[2];
+                if (setupBoard[pieceToAdd.row][pieceToAdd.col] == '_' || setupBoard[pieceToAdd.row][pieceToAdd.col] == ' ') {
+                    setupBoard[pieceToAdd.row][pieceToAdd.col] = pieceSymbol;
+                    // display the board
+                    displayBoard(setupBoard);
+                } else {
+                    cout << "the position is already occupied" << endl;
+                }
+            } catch (const invalid_argument&) {
+                cout << "invalid command in setup mode" << endl;
+            }
+        } else {
+            cout << "invalid command in setup mode" << endl;
+        }
+    }
+}
+
+void Game::moveCommand(string line) {
+    State currentState = board.getGameState();
+    Player* currentPlayer = (currentState.turn == Colour::White) ? whitePlayer.get() : blackPlayer.get();
+    
+    if (currentPlayer->isValidCommand(line)) {
+        Move move = currentPlayer->makeMove(board, line);
+        
+        // check if the move is legal
+        if(board.movePiece(move)) {
+            // successful move - display the board
+            cout << *textDisplay << endl;
+        } else {
+            cout << "illegal move" << endl;
+        }
+    } else {
+        cout << "invalid move command" << endl;
+    }
+}
+
+void Game::startGame(string line) {
+    size_t spacePos = line.rfind(' ');
+    string whitePlayerType;
+    string blackPlayerType;
+    bool validPlayers = true;
+
+    // read the player types
+    if (spacePos == string::npos) {
+        cout << "invalid game command" << endl;
+    } else {
+        whitePlayerType = line.substr(5, spacePos - 5);
+        blackPlayerType = line.substr(spacePos + 1);
+    }
+
+    // initialize white player
+    if(whitePlayerType == "human") {
+        whitePlayer = make_unique<HumanPlayer>();
+    } else if (whitePlayerType == "computer[1]") {
+        unique_ptr<Strategy> strategy = make_unique<Level1>();
+        whitePlayer = make_unique<ComputerPlayer>(move(strategy));
+    } else if (whitePlayerType == "computer[2]") {
+        unique_ptr<Strategy> strategy = make_unique<Level2>();
+        whitePlayer = make_unique<ComputerPlayer>(move(strategy));
+    } else if (whitePlayerType == "computer[3]") {
+        unique_ptr<Strategy> strategy = make_unique<Level3>();
+        whitePlayer = make_unique<ComputerPlayer>(move(strategy));
+    } else if (whitePlayerType == "computer[4]") {
+        // add code here for level 4
+        cout << "level 4 is not supported yet" << endl;
+        validPlayers = false;
+    } else {
+        cout << "invalid player type" << endl;
+        validPlayers = false;
+    }
+
+    // initialize black player
+    if (blackPlayerType == "human") {
+        blackPlayer = make_unique<HumanPlayer>();
+    } else if (blackPlayerType == "computer[1]") {
+        unique_ptr<Strategy> strategy = make_unique<Level1>();
+        blackPlayer = make_unique<ComputerPlayer>(move(strategy));
+    } else if (blackPlayerType == "computer[2]") {
+        unique_ptr<Strategy> strategy = make_unique<Level2>();
+        blackPlayer = make_unique<ComputerPlayer>(move(strategy));
+    } else if (blackPlayerType == "computer[3]") {
+        unique_ptr<Strategy> strategy = make_unique<Level3>();
+        blackPlayer = make_unique<ComputerPlayer>(move(strategy));
+    } else if (blackPlayerType == "computer[4]") {
+        // add code here for level 4
+        cout << "level 4 is not supported yet" << endl;
+        validPlayers = false;
+    } else {
+        cout << "invalid player type" << endl;
+        validPlayers = false;
+    }
+    if (validPlayers) {
+        // start the game
+        if (useDefaultBoard) {// if the user didn't use setup mode, we set up the default board
+            board.setupDefaultBoard();
+        }
+        isRunning = true;
+        // Display the initial board
+        cout << *textDisplay << endl;
+    }
+
+}
+
 void Game::gameRun() { 
     cout << "Hello World!" << endl; // to be removed
     cout << "Test" << endl; // to be removed
@@ -55,137 +257,15 @@ void Game::gameRun() {
 
     while (getline(cin, line)) {
         if (line == "resign" && isRunning) {
-            //add code here
-            State currentState = board.getGameState();
-            if (currentState.turn == Colour::White) {
-                cout << "Black wins!" << endl;
-                scoreboard.addBlackScore(1);
-            } else {
-                cout << "White wins!" << endl;
-                scoreboard.addWhiteScore(1);
-            }
-            isRunning = false;
-            useDefaultBoard = true;
-
+            resigning();
         } else if (line == "resign" && !isRunning) {
             cout << "can't resign, because game is not running" << endl;
         } else if (line == "setup" && !isRunning) {
-            // setup mode
-            Colour turn = Colour::White;
-            CastlingInfo castlingRights{false, false, false, false};
-            Posn enPassantTarget{-1, -1};
-            vector<vector<char>> setupBoard(8, vector<char>(8));
-            // initialize the empty board
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if ((i + j) % 2 == 0) {
-                        setupBoard[i][j] = ' ';
-                    } else {
-                        setupBoard[i][j] = '_'; // black space
-                    }
-                }
-            }
-            displayBoard(setupBoard);
-            while (getline(cin, line)) {                
-                if (line == "done") { // if requirements are met, we can exit setup mode and start the game
-                    // we assume castling and en passant are invalid
-                    
-                    // Count kings on the board
-                    int whiteKingCount = 0;
-                    int blackKingCount = 0;
-                    for (int i = 0; i < 8; i++) {
-                        for (int j = 0; j < 8; j++) {
-                            if (setupBoard[i][j] == 'K') whiteKingCount++;
-                            if (setupBoard[i][j] == 'k') blackKingCount++;
-                        }
-                    }
-
-                    if (whiteKingCount == 1 && blackKingCount == 1) {// first check if both kings are present
-                        Board tempBoard;
-                        State state{turn, GameStatus::IN_PROGRESS, castlingRights, enPassantTarget, setupBoard};
-                        tempBoard.changeState(state);
-                        if (tempBoard.isCheck(Colour::White) || tempBoard.isCheck(Colour::Black)) {
-                            cout << "invalid board, can't exit. at least one king is in check" << endl;
-                        } else {
-                            bool valid = true;
-                            for (int i = 0; i < 8; i++) {
-                                if (setupBoard[0][i] == 'p' || setupBoard[0][i] == 'P' || setupBoard[7][i] == 'p' || setupBoard[7][i] == 'P') {
-                                    cout << "invalid board, can't exit. pawns are not allowed in the first or last row" << endl;
-                                    valid = false;
-                                    break;
-                                }
-                            }
-                            if (valid) {
-                                // all requirements are met, we can exit setup mode and start the game
-                                board.changeState(state);
-                                useDefaultBoard = false;
-                                break;
-                            }    
-                        }
-                    } else {
-                        cout << "invalid board, can't exit. Must have exactly one king of each color (found " 
-                             << whiteKingCount << " white kings, " << blackKingCount << " black kings)" << endl;
-                    }
-                } else if (line == "= black") {
-                    turn = Colour::Black;
-                } else if (line == "= white") {
-                    turn = Colour::White;
-                } else if (line[0] == '-') {
-                    try {
-                        Posn pieceToRemove = convertToPosn(line.substr(2, 4));
-                        if (setupBoard[pieceToRemove.row][pieceToRemove.col] != '_' && setupBoard[pieceToRemove.row][pieceToRemove.col] != ' ') {
-                            if ((pieceToRemove.row + pieceToRemove.col) % 2 == 0) {
-                                setupBoard[pieceToRemove.row][pieceToRemove.col] = ' ';
-                            } else {
-                                setupBoard[pieceToRemove.row][pieceToRemove.col] = '_'; // black space
-                            }
-                            // display the board
-                            displayBoard(setupBoard);
-                        } else {
-                            cout << "the position is already empty" << endl;
-                        }
-                    } catch (const invalid_argument&) {
-                        cout << "invalid command in setup mode" << endl;
-                    }
-                } else if (line[0] == '+') {
-                    try {
-                        Posn pieceToAdd = convertToPosn(line.substr(4, 6));
-                        char pieceSymbol = line[2];
-                        if (setupBoard[pieceToAdd.row][pieceToAdd.col] == '_' || setupBoard[pieceToAdd.row][pieceToAdd.col] == ' ') {
-                            setupBoard[pieceToAdd.row][pieceToAdd.col] = pieceSymbol;
-                            // display the board
-                            displayBoard(setupBoard);
-                        } else {
-                            cout << "the position is already occupied" << endl;
-                        }
-                    } catch (const invalid_argument&) {
-                        cout << "invalid command in setup mode" << endl;
-                    }
-                } else {
-                    cout << "invalid command in setup mode" << endl;
-                }
-            }
-            //the setup mode is done, we can start the game
+            setupMode(line);
         } else if (line == "setup" && isRunning) {
             cout << "can't enter setup mode, because game is running" << endl;
         } else if (line.substr(0, 4) == "move" && isRunning) {// the move command
-            State currentState = board.getGameState();
-            Player* currentPlayer = (currentState.turn == Colour::White) ? whitePlayer.get() : blackPlayer.get();
-            
-            if (currentPlayer->isValidCommand(line)) {
-                Move move = currentPlayer->makeMove(board, line);
-                
-                // check if the move is legal
-                if(board.movePiece(move)) {
-                    // successful move - display the board
-                    cout << *textDisplay << endl;
-                } else {
-                    cout << "illegal move" << endl;
-                }
-            } else {
-                cout << "invalid move command" << endl;
-            }
-
+            moveCommand(line);
         } else if (line.substr(0, 4) == "move" && !isRunning) {
             cout << "can't move, because game is not running" << endl;
         } else if (line.substr(0, 4) == "undo" && isRunning) {
@@ -195,70 +275,7 @@ void Game::gameRun() {
         } else if (line.substr(0, 4) == "game" && isRunning) {
             cout << "can't start a new game, because currentgame is running" << endl;
         } else if (line.substr(0, 4) == "game" && !isRunning) {
-            size_t spacePos = line.rfind(' ');
-            string whitePlayerType;
-            string blackPlayerType;
-            bool validPlayers = true;
-
-            // read the player types
-            if (spacePos == string::npos) {
-                cout << "invalid game command" << endl;
-            } else {
-                whitePlayerType = line.substr(5, spacePos - 5);
-                blackPlayerType = line.substr(spacePos + 1);
-            }
-
-            // initialize white player
-            if(whitePlayerType == "human") {
-                whitePlayer = make_unique<HumanPlayer>();
-            } else if (whitePlayerType == "computer[1]") {
-                unique_ptr<Strategy> strategy = make_unique<Level1>();
-                whitePlayer = make_unique<ComputerPlayer>(move(strategy));
-            } else if (whitePlayerType == "computer[2]") {
-                unique_ptr<Strategy> strategy = make_unique<Level2>();
-                whitePlayer = make_unique<ComputerPlayer>(move(strategy));
-            } else if (whitePlayerType == "computer[3]") {
-                unique_ptr<Strategy> strategy = make_unique<Level3>();
-                whitePlayer = make_unique<ComputerPlayer>(move(strategy));
-            } else if (whitePlayerType == "computer[4]") {
-                // add code here for level 4
-                cout << "level 4 is not supported yet" << endl;
-                validPlayers = false;
-            } else {
-                cout << "invalid player type" << endl;
-                validPlayers = false;
-            }
-
-            // initialize black player
-            if (blackPlayerType == "human") {
-                blackPlayer = make_unique<HumanPlayer>();
-            } else if (blackPlayerType == "computer[1]") {
-                unique_ptr<Strategy> strategy = make_unique<Level1>();
-                blackPlayer = make_unique<ComputerPlayer>(move(strategy));
-            } else if (blackPlayerType == "computer[2]") {
-                unique_ptr<Strategy> strategy = make_unique<Level2>();
-                blackPlayer = make_unique<ComputerPlayer>(move(strategy));
-            } else if (blackPlayerType == "computer[3]") {
-                unique_ptr<Strategy> strategy = make_unique<Level3>();
-                blackPlayer = make_unique<ComputerPlayer>(move(strategy));
-            } else if (blackPlayerType == "computer[4]") {
-                // add code here for level 4
-                cout << "level 4 is not supported yet" << endl;
-                validPlayers = false;
-            } else {
-                cout << "invalid player type" << endl;
-                validPlayers = false;
-            }
-            if (validPlayers) {
-                // start the game
-                if (useDefaultBoard) {// if the user didn't use setup mode, we set up the default board
-                    board.setupDefaultBoard();
-                }
-                isRunning = true;
-                // Display the initial board
-                cout << *textDisplay << endl;
-            }
-
+            startGame(line);
         } else if (line.substr(0, 4) == "help") {
             cout << "this feature is not currently supported" << endl;
             // add code for bonus feature
--- game.h
+++ game.h
@@ -1,15 +1,22 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <iostream>
 #include <memory>
 #include <stack>
+#include <string>
+#include <vector>
 #include "board.h"
 #include "scoreboard.h"
 #include "player.h"
 #include "move.h"
 #include "textdisplay.h"
+#include "state.h"
+#include "posn.h"
+#include "castlinginfo.h"
 
 class Game {
+private:
     Board board;
     Scoreboard scoreboard;
     std::unique_ptr<Player> whitePlayer;
@@ -18,8 +25,14 @@ class Game {
     bool isRunning; // true if the game is running, false if the game is over
     bool useDefaultBoard; // false if the user uses setup mode.
     std::unique_ptr<TextDisplay> textDisplay; // Observer for board display
+
+    // Private Methods
     Posn convertToPosn(const std::string& posnStr);
     void displayBoard(const std::vector<std::vector<char>>& setupBoard);
+    void resigning();
+    void setupMode(std::string line);
+    void moveCommand(std::string line);
+    void startGame(std::string line);
 
 public:
     Game();
